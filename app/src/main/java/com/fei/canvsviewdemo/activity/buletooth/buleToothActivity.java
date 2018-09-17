5cfa7dc1eae251aab99d024e7c3ce476df9d727c
fei <931516018@qq.com>
2018-09-17 18:35:29 +0800

    增加忘提交的文件

---
+++ app/src/main/java/com/fei/canvsviewdemo/activity/buletooth/buleToothActivity.java
@@ -0,0 +1,21 @@
+package com.fei.canvsviewdemo.activity.buletooth;
+
+import com.fei.canvsviewdemo.R;
+import com.fei.feilibs_1_0_0.base.ac.BaseActivity;
+
+public class buleToothActivity extends BaseActivity {
+    @Override
+    protected int initLayout() {
+        return R.layout.activity_buletooth;
+    }
+
+    @Override
+    protected boolean isBackshow() {
+        return false;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
